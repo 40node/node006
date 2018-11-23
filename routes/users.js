55d24535ad3f99a91f9819ad0db0426d82dce8b7
sho kisaragi <tak@oshiire.to>
2018-11-23 14:55:31 +0900

    add user table and routing tables

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+// Controller を追加
+const userManager = require('../controllers/userManager');
+
+// http://users/ へアクセスしたとき → 一覧を表示する
+router.get('/', function (req, res) {
+  userManager.view(req, res);
+});
+
+// http://users/100 などでアクセスしたとき → 該当のID(この場合は100)の詳細を表示する
+router.get('/:id', function (req, res) {
+  userManager.find(req, res);
+});
+
+// http://users/register へ POST したとき → 本を新たに登録する
+router.post('/register', ((req, res) => {
+  userManager.register(req, res);
+}));
+
+// http://users/update/100 などで POST したとき → 該当のID(この場合は100)の内容を更新する
+router.post('/update/:id', ((req, res) => {
+  userManager.update(req, res);
+}));
+
+// http://users/destroy/100 などでアクセスしたとき → 該当のID(この場合は100)の本を削除する
+router.get('/destroy/:id', function (req, res) {
+  userManager.destroy(req, res);
+});
+
+module.exports = router;
